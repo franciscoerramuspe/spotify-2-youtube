2f8f149cbc234ac6b58bc96c40d6dd9796771e33
Francisco Erramuspe <franciscoerramuspe91@gmail.com>
2025-04-17 15:43:20 -0400

    deleted unnecesary folder

--- server/index.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-
-
-const app = express();
-app.get('/', (req, res) => res.send('Hello from your API'));
-const port = process.env.PORT || 4000;
-app.listen(port, () => console.log(`Listening on ${port}`));
